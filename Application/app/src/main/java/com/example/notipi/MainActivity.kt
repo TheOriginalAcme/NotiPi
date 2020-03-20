024c533e7c8ac2fd7f9aa2996f31da2a306aa21c
RonK <ronkup12@gmail.com>
2020-03-20 15:22:25 +0200

    Finished copying example - need to test and debug this shit...

--- Application/app/src/main/java/com/example/notipi/MainActivity.kt
+++ Application/app/src/main/java/com/example/notipi/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.notipi
 
 import android.annotation.SuppressLint
-import android.app.Notification
 import android.content.*
+import android.net.wifi.p2p.WifiP2pConfig
+import android.net.wifi.p2p.WifiP2pDevice
+import android.net.wifi.p2p.WifiP2pDeviceList
 import android.net.wifi.p2p.WifiP2pManager
+import android.os.AsyncTask
 import android.os.Build
 import android.os.Bundle
 import android.os.IBinder
@@ -13,14 +16,13 @@ import android.service.notification.StatusBarNotification
 import android.text.TextUtils
 import android.util.Log
 import android.view.View
-import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import java.net.ServerSocket
 
 
 @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
@@ -60,22 +62,91 @@ class MyNotificationListener : NotificationListenerService() {
 class MainActivity : AppCompatActivity() {
 
     private lateinit var nameInput: EditText
+    private val manager: WifiP2pManager? by lazy(LazyThreadSafetyMode.NONE) {
+        getSystemService(Context.WIFI_P2P_SERVICE) as WifiP2pManager?
+    }
+    var mChannel: WifiP2pManager.Channel? = null
+    var receiver: BroadcastReceiver? = null
+    val intentFilter = IntentFilter().apply {
+        addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION)
+        addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION)
+        addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION)
+        addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d("Yanai says", "We are up and running")
         setContentView(R.layout.activity_main)
+        Log.d("MainActivity", "We are up and running")
 
+        Log.d("MainActivity", "Checking that NotificationService is enabled")
         if (!isNotificationServiceEnabled())
         {
             buildNotificationServiceAlertDialog().show()
         }
 
-        Log.d("yanai says", "Finished on create")
+        Log.d("MainActivity", "Wifi P2P stuff...")
+        mChannel = manager?.initialize(this, mainLooper, null)
+        mChannel?.also { channel ->
+            receiver = WiFiDirectBroadcastReceiver(manager, channel, this)
+        }
+
+        manager?.discoverPeers(mChannel, object: WifiP2pManager.ActionListener {
+            override fun onSuccess() {
+                Log.d("MainActivity", "Successfully discovered Peers")
+            }
+
+            override fun onFailure(reasonCode: Int) {
+                Log.d("MainActivity", "Failed to discover Peers (reason: $reasonCode)")
+            }
+        })
+        lateinit var selectedDevice: WifiP2pDevice
+        manager?.requestPeers(mChannel, object: WifiP2pManager.PeerListListener {
+            override fun onPeersAvailable(peers: WifiP2pDeviceList?) {
+                Log.d("MainActivity", "Found available peers:")
+                Log.d("-> available peers:", peers?.deviceList.toString())
+                for (device in peers?.deviceList!!) {
+                    if (device.deviceName == "NotiPi") {
+                        Log.d("MainActivity", "Found NotiPi peer: $device")
+                        selectedDevice = device
+                    }
+                }
+            }
+        })
+        Log.d("MainActivity", "We found the device that you want to connect to")
+        val config = WifiP2pConfig()
+        config.deviceAddress = selectedDevice.deviceAddress
+        mChannel?.also { channel ->
+            manager?.connect(channel, config, object : WifiP2pManager.ActionListener {
+                override fun onSuccess() {
+                    Log.d("MainActivity", "Connected to NotiPi device (${config.deviceAddress})")
+                    Log.d("MainActivity", "Starting server...")
+                    DataServerAsyncTask(findViewById<TextView>(R.id.textView)).execute()
+                }
+
+                override fun onFailure(reason: Int) {
+                    Log.d("MainActivity", "Failed to connect to NotiPi device (${config.deviceAddress})")
+                }
+            }
+            )}
+
+        Log.d("MainActivity", "Finished on create")
+    }
+
+    /** register the BroadcastReceiver with the intent values to be matched  */
+    override fun onResume() {
+        super.onResume()
+        registerReceiver(receiver, intentFilter)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        unregisterReceiver(receiver)
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        unregisterReceiver(receiver)
     }
 
     fun getNotificationsPressed(view: View)
@@ -137,9 +208,9 @@ class MainActivity : AppCompatActivity() {
  * A BroadcastReceiver that notifies of important Wi-Fi p2p events.
  */
 class WiFiDirectBroadcastReceiver(
-    private val manager: WifiP2pManager,
-    private val channel: WifiP2pManager.Channel
-//    private val activity: MyWifiActivity
+    private val manager: WifiP2pManager?,
+    private val channel: WifiP2pManager.Channel,
+    private val activity: MainActivity
 ) : BroadcastReceiver() {
 
     @SuppressLint("LongLogTag")
@@ -151,15 +222,22 @@ class WiFiDirectBroadcastReceiver(
                 when (intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1)) {
                     WifiP2pManager.WIFI_P2P_STATE_ENABLED -> {
                         // Wifi P2P is enabled
+                        Log.d("WifiDirectBroadcastReceiver", "-> Wifi P2P is enabled")
                     }
                     else -> {
                         // Wi-Fi P2P is not enabled
+                        Log.d("WifiDirectBroadcastReceiver", "-> Wifi P2P is not enabled")
                     }
                 }
             }
             WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION -> {
                 // Call WifiP2pManager.requestPeers() to get a list of current peers
                 Log.d("WifiDirectBroadcastReceiver", "Wifi peers changed")
+                manager?.requestPeers(channel) { peers: WifiP2pDeviceList? ->
+                    // Handle peers list
+                    Log.d("WifiDirectBroadcastReceiver", "-> Peers list:")
+                    Log.d(" -> Peers List", peers.toString())
+                }
             }
             WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION -> {
                 // Respond to new connection or disconnections
@@ -172,6 +250,44 @@ class WiFiDirectBroadcastReceiver(
                 // Applications can use requestDeviceInfo() to retrieve the current connection information.
                 Log.d("WifiDirectBroadcastReceiver", "device's wifi state changed")
             }
+        }
+    }
+}
+
+class DataServerAsyncTask(
+    private var statusText: TextView
+) : AsyncTask<Void, Void, String?>() {
+
+    override fun doInBackground(vararg params: Void): String? {
+        /**
+         * Create a server socket.
+         */
+        val serverSocket = ServerSocket(8888)
+        Log.d("DataServer", "Socket opened")
+        return serverSocket.use {
+            /**
+             * Wait for client connections. This call blocks until a
+             * connection is accepted from a client.
+             */
+            val client = serverSocket.accept()
+            Log.d("DataServer", "Connection done")
+            /**
+             * If this code is reached, a client has connected and transferred data
+             * Save the input stream from the client
+             */
+            val inputStream = client.getInputStream()
+            Log.d("DataServer", inputStream.toString())
+            serverSocket.close()
+            "Data: $inputStream"
+        }
+    }
+
+    /**
+     * Start activity that can handle input stream
+     */
+    override fun onPostExecute(result: String?) {
+        result?.run {
+            statusText.text = "Data received - $result"
         }
     }
 }
